ca52cfa2c2a05f69d3f72897afa783e49f5b1110
Davidson Fellipe <email@fellipe.com>
2014-08-28 23:22:56 -0300

    sample js files

---
+++ src/js/a.js
@@ -0,0 +1,23 @@
+/*global $:false, setTimeout:false, document:false, jQuery: false */
+
+(function (window, $) {
+
+    "use strict";
+
+    var AAnIncrediblyLargeClassName = function (options) {
+
+        this.options = options;
+
+    };
+
+    AAnIncrediblyLargeClassName.prototype.init = function() {
+
+        var anIncrediblyLargeVariableName = 0;
+
+        return anIncrediblyLargeVariableName;
+
+    };
+
+    window.AAnIncrediblyLargeClassName = AAnIncrediblyLargeClassName;
+
+})(window, jQuery);
---
+++ src/js/b.js
@@ -0,0 +1,25 @@
+/*global $:false, setTimeout:false, document:false, jQuery: false */
+
+(function (window, $) {
+
+    "use strict";
+
+    anIncrediblyLargeClassName
+
+    var BAnIncrediblyLargeClassName = function (options) {
+
+        this.options = options;
+
+    };
+
+    BAnIncrediblyLargeClassName.prototype.init = function() {
+
+        var anIncrediblyLargeVariableName = 0;
+
+        return anIncrediblyLargeVariableName;
+
+    };
+
+    window.BAnIncrediblyLargeClassName = BAnIncrediblyLargeClassName;
+
+})(window, jQuery);
