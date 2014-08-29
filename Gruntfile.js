1cc97fe6389ce10acb266c26da001779df449ec3
Davidson Fellipe <email@fellipe.com>
2014-08-28 23:26:40 -0300

    time-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 module.exports = function(grunt) {
 
   // measures the time each task takes
-  // require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 
+  // load-grunt-config includes load-grunt-tasks
   require('load-grunt-config')(grunt);
 };
