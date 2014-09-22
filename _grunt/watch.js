92a8ddd7d4533791d9e8d339ee8b04d7cbb67824
Davidson Fellipe <email@fellipe.com>
2014-09-21 23:10:34 -0300

    adding new task to grunt watch

--- _grunt/watch.js
+++ _grunt/watch.js
@@ -1,8 +1,13 @@
 // Hint: newer:taskName - configure Grunt tasks to run with newer files only.
 module.exports = {
   scripts: {
-      files: ['<%= path.src %>js/*.js', '<%= path.src %>scss/*.scss'],
-      tasks: ['concurrent:join', 'concurrent:lint'],
+      files: ['<%= path.src %>js/*.js',
+              '<%= path.src %>scss/*.scss',
+              '<%= path.src %>*.html'],
+      tasks: ['concurrent:join',
+              'concurrent:lint',
+              'concurrent:optim',
+              'concurrent:copy'],
       options: {
           nospawn: true,
           debounceDelay: 250,
