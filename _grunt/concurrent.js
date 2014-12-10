947191d95669d82a167bffb87b517fa48fbd5936
Davidson Fellipe <email@fellipe.com>
2014-12-10 12:07:13 -0200

    removing newer on scss files

--- _grunt/concurrent.js
+++ _grunt/concurrent.js
@@ -1,5 +1,5 @@
 module.exports = {
-  join: ['newer:sass:dev', 'newer:concat'],
+  join: ['sass:dev', 'newer:concat'],
   lint: ['newer:jshint', 'newer:csslint'],
   optim: ['newer:uglify', 'newer:imagemin', 'newer:htmlmin'],
   copy: ['newer:copy']
